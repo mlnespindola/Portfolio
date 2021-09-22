a1713c7de97c1fd191e0934796afb745493d0994
milena espindola <espindolamilena@gmail.com>
2021-09-22 20:06:34 -0300

    Topbar
    navigation links

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
-import './topbar.scss';
+import "./topbar.scss";
+import { Person, Email } from "@material-ui/icons";
 
 export default function Topbar() {
-    return (
-        <div className='topbar'>
-
+  return (
+    <div className="topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            frontend.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+55 27 99603432</span>
+          </div>
+          <div className="itemContainer">
+            <Email className="icon"/>
+            <span>espindolamilena@gmail.com</span>
+          </div>
         </div>
-    );
+
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
