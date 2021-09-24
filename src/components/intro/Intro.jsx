a5cecc87eb81e9c2e9917707a77d23bc5709ecd0
milena espindola <espindolamilena@gmail.com>
2021-09-23 22:07:56 -0300

    Introduction Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,23 @@
-import './intro.scss';
+import "./intro.scss";
 
 export default function Intro() {
-    return (
-        <div className='intro' id='intro'>
-            
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="imgs/man.png" alt="Milena Espíndola"></img>
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Olá, eu sou a</h2>
+          <h1>Milena Espíndola</h1>
+          <h3>freelancer</h3>
+          <a href="#portfolio">
+              <img src="imgs/down.png" alt="seta" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
